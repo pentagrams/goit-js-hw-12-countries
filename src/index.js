02d0c722930ca9b8c4004791be2baeb3ffac776e
Max <maxminullin@gmail.com>
2021-03-21 22:20:11 +0200

    v0.2

--- src/index.js
+++ src/index.js
@@ -1,35 +1,71 @@
 import './styles.css';
-const debounce = require('lodash.debounce');
-import API from './js/fetchCountries';
-import CountryCardTpl from './tpl/CountryCardTpl.hbs'
+import 'lodash';
+import fetchCountries from './js/fetchCountries';
+import countryCardTpl from './templates/CountryCardTpl.hbs'
+import countriesListTpl from './templates/list.hbs'
+const { error }  = require('@pnotify/core');
+
 
 
 const refs = {
       cardContainer: document.querySelector('.js-card-container'),
       searchForm: document.querySelector('.js-search-form'),
-
 }
 
-refs.searchForm.addEventListener('input', onSearch)
+
+
+refs.searchForm.addEventListener('input', _.debounce(onSearch, 500));
+
 
 function onSearch(event) {
       event.preventDefault();
+      clearCountriesList()
 
-      const form = event.currentTarget;
-      const searchQuery = form.elements.query.value;
+      // const form = event.currentTarget;
+      const searchQuery = form.elements.query.value.trim();
+      
+      if (searchQuery.length === 0) {
+            return
+      } else {
+           return fetchCountries(searchQuery)
+            .then(findDesirableCountry)
+            .catch(onFetchError); 
+      }
+}
 
-      API.fetchCountries(searchQuery)
-            .then(renderCountryCard)
-            .catch(onFetchError);
+function onFetchError(error) {
+      console.log('Error!', error);
 }
 
 function renderCountryCard(country) {
-      const markup = CountryCardTpl(country);
-      refs.cardContainer.innerHTML = markup;
+      refs.cardContainer.insertAdjacentHTML("beforeend",countryCardTpl(country))
+}
+
+function renderCountriesList(countries) {
+    refs.cardContainer.insertAdjacentHTML("beforeend",countriesListTpl(countries))
 }
 
 
-function onFetchError(error) {
-      alert('Ошибка!')
+function clearCountriesList() {
+    refs.countriesContainer.innerHTML = '';
 }
 
+function findDesirableCountry(data) {
+      if (data.length > 10) {
+            error({
+                  delay: 1000,
+                  text: 'Too many matches found. Please try a more specific query',
+                  type: 'info'
+            });
+      } else if (data.length === 1) {
+            renderCountryCard(data);
+      } else if (ata.length === undefined) {
+            error({
+                delay: 1000,
+                text: 'Incorrect name of the counrty. Please check and try again',
+                type: 'info'
+                });
+      } else {
+            renderCountriesList(data);
+      }
+}
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,7 +1,10 @@
-const BASE_URL = 'https://restcountries.eu/rest/v2';
 
-function fetchCountries(name) {
-      return fetch(`${BASE_URL}/${name}`).then(response => response.json());
+export default  function fetchCountries(searchQuery) {
+      const option = {
+        headers: {}
+      }
+      const URL = `https://restcountries.eu/rest/v2/name/${searchQuery}`
+      return fetch(URL, option)
+            .then(response => response.json())
+            .then(data => data);
 }
-
-export default {fetchCountries}
